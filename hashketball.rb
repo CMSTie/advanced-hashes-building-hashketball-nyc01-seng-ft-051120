8fe02827c976b2780baed6b26d6d471b3f255ced
Chris Hosler <cmstie@gmail.com>
2020-04-22 21:49:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,11 @@
 def game_hash
-  {:home => {:team_name => "", :colors => [], :players => [{}] }, 
+  {:home => {:team_name => "",
+  :colors => [], 
+  :players => [{:player_name => }, {:number => }, {:shoe => }, {:points => }, {:rebounds => }] }, 
+  
+  
+  
+  
   :away => {:team_name => "", :colors => [], :players => [{}] }
   }
 end
